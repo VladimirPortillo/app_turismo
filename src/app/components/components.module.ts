eb70341fc80166c45eeadc4ba072452c9b365d83
cllanosucb <cllanos@ucb.edu.bo>
2024-11-18 10:43:07 -0400

    Mapa funcionando, pequeño ejemplo

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,21 +1,13 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppBarComponent } from './app-bar/app-bar.component';
 
 
 
 @NgModule({
-  declarations: [
-    AppBarComponent
-  ],
-  exports:[
-    AppBarComponent
-  ],
-
-
-  imports: [
-    CommonModule
-  ]
+  declarations: [AppBarComponent],
+  exports: [AppBarComponent],
+  imports: [CommonModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-
-export class ComponentsModule { }
+export class ComponentsModule {}
--- src/app/pages/mapa/mapa.page.ts
+++ src/app/pages/mapa/mapa.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,  ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit,  ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { GoogleMap } from '@capacitor/google-maps';
 import { MenuController } from '@ionic/angular';
 import { environment } from 'src/environments/environment.prod';
@@ -11,16 +11,43 @@ declare const google: any;
   templateUrl: './mapa.page.html',
   styleUrls: ['./mapa.page.scss'],
 })
-export class MapaPage implements OnInit {
-
-  @ViewChild('map')
-  mapRef!: ElementRef<HTMLElement>;
+export class MapaPage implements OnInit, AfterViewInit {
+  @ViewChild('map') mapRef!: ElementRef<HTMLElement>;
   newMap!: GoogleMap;
 
-  constructor(private menuController:MenuController){
-   }
+  datosMarkers: any[] = [];
+  center: any = { lat: 40.7128, lng: -74.006 };
+  loading: boolean = true;
+
+  constructor(private menuController: MenuController) {}
 
   ngOnInit() {
+    console.log('ngOnInit');
+  }
+
+  // Se ejecuta después de que el DOM y las vistas estén listas
+  async ngAfterViewInit() {
+    console.log('cargando mapa ....');
+    await setTimeout(async () => {
+      this.datosMarkers.push({
+        id_comunidad: 1,
+        nombre: 'Comarapa',
+        longitud: -117.8,
+        latitud: 33.5,
+      });
+      this.datosMarkers.push({
+        id_comunidad: 2,
+        nombre: 'Comarapa 2',
+        longitud: -117.7,
+        latitud: 33.4,
+      });
+      this.loading = false;
+      console.log('2 ==> ', this.datosMarkers);
+      await this.createMap();
+    }, 5000);
+    //this.loading = false;
+    //this.loadingMap = true;
+    //await this.createMap();
   }
 
   async createMap() {
@@ -36,6 +63,24 @@ export class MapaPage implements OnInit {
         zoom: 8,
       },
     });
+
+    this.markersMap();
+  }
+
+  async markersMap() {
+    const mapMarkers = this.datosMarkers.map(d => {
+      return {
+        coordinate: {
+          lat: d.latitud,
+          lng: d.longitud,
+        },
+        title: d.nombre,
+      };
+    });
+    
+    console.log('mapMarkers', mapMarkers);    
+    const markers = await this.newMap.addMarkers(mapMarkers);
+    console.log('markers', markers);    
   }
 
 }
